83bf86f0d463d876f1a9dd1b457f1fa6b5c993c6
03517746 <lluis.moreso@mango.com>
2020-03-05 15:39:37 +0100

    afegit server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+    open: true,
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
